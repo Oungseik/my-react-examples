713b3b301d13e98578372483e7608b3040be4774
Oungseik <mhemaungthuwin@gmail.com>
2022-02-04 17:46:42 +0630

    [feature] - add rating feature on colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import { useState } from "react";
+import colorData from './data/color-data.json';
+import ColorList from "./components/ColorList";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+  const [colors, setColors] = useState(colorData);
+
+  const removeColorHandler = (id: string) => {
+    const newColors = colors.filter(color => color.id !== id);
+    setColors(newColors);
+  }
+
+  const rateColorHandler = (id: string, rating: number) => {
+    const newColors = colors.map(color => color.id === id ? { ...color, rating } : color)
+    setColors(newColors);
+
+  }
+
+  return <div className="App">
+    <ColorList colors={colors} onRemoveColor={id => removeColorHandler(id)} onRateColor={rateColorHandler} />
+  </div>;
 }
 
 export default App;
