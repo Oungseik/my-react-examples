b68524f3ed1e62ce01fb2cc299d82a37311d25ce
Oungseik <mhemaungthuwin@gmail.com>
2022-02-05 12:30:32 +0630

    [refactor] - use custom useInput hook

--- src/components/Form/ControlledAddColorForm.tsx
+++ src/components/Form/ControlledAddColorForm.tsx
@@ -1,24 +1,23 @@
-import { useState } from "react";
+import { useInput } from "../../hooks";
 
 interface AddColorFormProps {
   onNewColor: (title: string, color: string) => void;
 }
 
 export default function AddColorForm({ onNewColor = f => f }: AddColorFormProps) {
-  const [title, setTitle] = useState("");
-  const [color, setColor] = useState("#000000");
+  const [titleProps, resetTitle] = useInput("");
+  const [colorProps, resetColor] = useInput("#000000");
 
   const submit = (e: React.FormEvent) => {
     e.preventDefault();
-    onNewColor(title, color);
-    setTitle("");
-    setColor("");
+    resetTitle();
+    resetColor();
   }
 
   return (
     <form onSubmit={submit}>
-      <input value={title} onChange={e => setTitle(e.target.value)} type="text" placeholder="color title" required />
-      <input value={color} onChange={e => setColor(e.target.value)} type="color" required />
+      <input {...titleProps} type="text" placeholder="color title" required />
+      <input {...colorProps} type="color" required />
       <button>ADD</button>
     </form>
   )
